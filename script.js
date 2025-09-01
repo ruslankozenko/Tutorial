2bd95b9f743d452f9bb384286b18dbd99f32378f
ruslankozenko <kozenko.r@seznam.cz>
2025-09-01 18:11:53 +0200

    delete

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const div = document.createElement('div');
-const header = document.createElement('h1');
 
-header.innerText = 'Learn JS'
-header.classList.add('test')
-const a = document.createElement('a');
-a.href = 'https://developer.mozilla.org/ru/docs/Web/JavaScript'
-a.innerText = 'MDN'
-
-div.appendChild(header)
-div.appendChild(a)
-document.body.appendChild(div)
-
-console.log(div)
-console.log(header)
-console.log(a)
