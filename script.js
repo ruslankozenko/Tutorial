b4b36eb1f058ede78039ab7faef5772c2371d32e
ruslankozenko <kozenko.r@seznam.cz>
2025-09-11 19:10:52 +0200

    js off

--- script.js
+++ script.js
@@ -77,3 +77,36 @@
 // <!-- Образец 2/-->
 
 // <!-- Образец 3-->
+
+const minus = document.querySelector('#minus')
+const plus = document.querySelector('#plus')
+const normal = document.querySelector('#normal')
+const bold = document.querySelector('#bold')
+const italic = document.querySelector('#italic')
+const text = document.querySelector('h3')
+
+const styles = window.getComputedStyle(text)
+let fontSize = parseInt(styles.fontSize)
+
+minus.addEventListener('click', () => {
+    fontSize--
+    text.style.fontSize = fontSize + 'px'      
+})
+
+plus.addEventListener('click', () => {
+    fontSize++
+    text.style.fontSize = fontSize + 'px'      
+})
+
+bold.addEventListener('click', () => {
+    text.style.fontWeight = 'bold'    
+})
+
+italic.addEventListener('click', () => {
+    text.style.fontStyle = 'italic'    
+})
+
+normal.addEventListener('click', () => {
+    text.style.fontStyle = 'normal'
+    text.style.fontWeight = 'normal' 
+})
