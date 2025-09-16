1df7b5ec9f258aff58ddf2fbf03a128abed4ad05
ruslankozenko <kozenko.r@seznam.cz>
2025-09-16 17:44:39 +0200

    example 4

--- script.js
+++ script.js
@@ -76,37 +76,68 @@
 
 // <!-- Образец 2/-->
 
-// <!-- Образец 3-->
+//   Образец 3
 
-const minus = document.querySelector('#minus')
-const plus = document.querySelector('#plus')
-const normal = document.querySelector('#normal')
-const bold = document.querySelector('#bold')
-const italic = document.querySelector('#italic')
+// const minus = document.querySelector('#minus')
+// const plus = document.querySelector('#plus')
+// const normal = document.querySelector('#normal')
+// const bold = document.querySelector('#bold')
+// const italic = document.querySelector('#italic')
+// const text = document.querySelector('h3')
+
+// const styles = window.getComputedStyle(text)
+// let fontSize = parseInt(styles.fontSize)
+
+// minus.addEventListener('click', () => {
+//     fontSize--
+//     text.style.fontSize = fontSize + 'px'      
+// })
+
+// plus.addEventListener('click', () => {
+//     fontSize++
+//     text.style.fontSize = fontSize + 'px'      
+// })
+
+// bold.addEventListener('click', () => {
+//     text.style.fontWeight = 'bold'    
+// })
+
+// italic.addEventListener('click', () => {
+//     text.style.fontStyle = 'italic'    
+// })
+
+// normal.addEventListener('click', () => {
+//     text.style.fontStyle = 'normal'
+//     text.style.fontWeight = 'normal' 
+// })
+
+//  Образец №3/
+
+//  Образец №4
+
+const panel = document.querySelector('#panel')
 const text = document.querySelector('h3')
 
 const styles = window.getComputedStyle(text)
 let fontSize = parseInt(styles.fontSize)
 
-minus.addEventListener('click', () => {
-    fontSize--
-    text.style.fontSize = fontSize + 'px'      
+panel.addEventListener('click', event => {
+    const type = event.target.dataset.type
+
+    if (type === 'minus') {
+        fontSize--
+        text.style.fontSize = fontSize + 'px'
+    } else if (type === 'plus') {
+        fontSize++
+        text.style.fontSize = fontSize + 'px'
+    } else if (type === 'normal') {
+        text.style.fontWeight = 'normal'
+        text.style.fontStyle = 'normal'
+    } else if (type === 'bold') {
+        text.style.fontWeight = 'bold'
+    } else if (type === 'italic') {
+        text.style.fontStyle = 'italic'
+    }
 })
 
-plus.addEventListener('click', () => {
-    fontSize++
-    text.style.fontSize = fontSize + 'px'      
-})
-
-bold.addEventListener('click', () => {
-    text.style.fontWeight = 'bold'    
-})
-
-italic.addEventListener('click', () => {
-    text.style.fontStyle = 'italic'    
-})
-
-normal.addEventListener('click', () => {
-    text.style.fontStyle = 'normal'
-    text.style.fontWeight = 'normal' 
-})
+//  Образец №4/
